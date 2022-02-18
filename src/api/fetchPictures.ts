4f42e1c7b6a62416bcc6cc57b34860ae1ea2281a
Vadim <magnalov@mail.ru>
2022-02-18 16:39:10 +0300

    api updated

--- src/api/fetchPictures.ts
+++ src/api/fetchPictures.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
-export default async function fetchPictures(page: number = 1) {
+export default async function fetchPictures(
+  page: number = 1,
+  albumId?: number
+) {
   const resp = await axios.get(
-    `http://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=20`
+    `http://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=40&${
+      albumId && `albumId=${albumId}`
+    }`
   );
   return resp.data;
 }
